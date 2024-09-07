fe15afe177e4ce8b90596e7cc2cae9316d6ef599
roelckers <roelckers@miuandes.cl>
2024-09-06 20:00:16 -0400

    Lab 5 al 94%

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
+    @editable = current_user == @user
   end
 
   def edit
